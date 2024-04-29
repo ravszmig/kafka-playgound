f61cb826c2852b66176dbf151b7483a08b71e82b
ravszmig <rafal.szmig@wp.pl>
2024-04-29 12:14:31 +0200

    Initial commit

---
+++ src/main/java/com/ravsky/kafkaplaygound/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package com.ravsky.kafkaplaygound.constant;
+
+public class AppConstant {
+    public static final String CAB_LOCATION = "cab-location";
+}
+
+
---
+++ src/test/java/com/ravsky/kafkaplaygound/KafkaPlaygoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravsky.kafkaplaygound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
