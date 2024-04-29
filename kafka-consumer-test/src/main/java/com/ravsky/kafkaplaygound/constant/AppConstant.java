ccbcea41535b402b3fcc612ee6abf6691b3bdf12
ravszmig <rafal.szmig@wp.pl>
2024-04-29 13:02:04 +0200

    Consumer

---
+++ kafka-consumer-test/src/main/java/com/ravsky/kafkaplaygound/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package main.java.com.ravsky.kafkaplaygound.constant;
+
+public class AppConstant {
+    public static final String CAB_LOCATION = "cab-location";
+}
+
+
---
+++ kafka-consumer-test/src/test/java/com/ravsky/kafkaplaygound/KafkaPlaygoundApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.ravsky.kafkaplaygound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPlaygoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
